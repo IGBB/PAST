667a1ce854613ca7263ddbc0ca314db2bc36e98a
HugeLegolas <39808987+HugeLegolas@users.noreply.github.com>
2018-06-07 12:49:55 -0500

    Trying to merge rows

--- Really awesome stuff.R
+++ Really awesome stuff.R
@@ -1,1 +1,9 @@
+oddvals <- seq(1, nrow(GrainColorGLMAllEff), by = 2)
+evenvals <- seq(2, nrow(GrainColorGLMAllEff), by = 2)
+rows = nrow(GrainColorGLMAllEff)
+
+for(i in 1:rows)
+  merge(GrainColorGLMAllEff[oddvals[i], ], GrainColorGLMAllEff[evenvals[i], ], by = "Marker")
+
+
 GrainColorGLMStats <- GrainColorGLMStats[!(GrainColorGLMStats$marker_F == "NaN"), ]
