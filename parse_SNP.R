558a218140d43dc124a796fdf417c968ef935cec
HugeLegolas <39808987+HugeLegolas@users.noreply.github.com>
2018-06-13 12:00:34 -0500

    Putting code in a loop

--- parse_SNP.R
+++ parse_SNP.R
@@ -1,5 +1,16 @@
 library(tidyverse)
 LD_file <- parse_LD("example/KernelColorLD.1.txt.gz")
 
-LD_file1 <- LD_file[[1]][["1"]]
-testfile <- LD_file[(LD_file[[1]][["1"]][["Position1"]] %in% all_data$Pos)]
+#Give upstream and downstream their own variables to make life easier
+LD_upstream <- LD_file[[1]]
+LD_downstream <- LD_file[[2]]
+
+
+
+
+for (name in names(LD_upstream)){
+  temp_data <- LD_upstream[[name]]
+  temp_data <- temp_data[(temp_data$Position1 %in% all_data$Pos),]
+}
+LD_file <- LD_file[[1]][["1"]]
+LD_file <- LD_file[(LD_file$Position1 %in% all_data$Pos),]
