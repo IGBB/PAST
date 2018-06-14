63777b50e7de00da8376f1fb1bbf883b123dae26
Adam Thrash <cathrash92@gmail.com>
2018-06-14 13:00:11 -0500

    stylistic changes and moving to correct directory

---
+++ R/parse_SNP.R
@@ -0,0 +1,21 @@
+parse_SNP <- function(LD_file) {
+
+  #Give upstream and downstream their own variables to make life easier
+  LD_upstream <- LD_file[[1]]
+  LD_downstream <- LD_file[[2]]
+
+
+
+  for (name in names(LD_upstream)){
+    temp_data <- LD_upstream[[name]]
+    LD_upstream[[name]] <- temp_data[(temp_data$Position1 %in% all_data$Pos),]
+  }
+
+  for (name in names(LD_downstream)){
+    temp_data <- LD_downstream[[name]]
+    LD_downstream[[name]] <- temp_data[(temp_data$Position1 %in% all_data$Pos),]
+  }
+
+
+  list(LD_upstream, LD_downstream)
+}
