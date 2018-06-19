f511853303d8ba214f201d3d4c4e7d4a61c1a75e
Adam Thrash <cathrash92@gmail.com>
2018-06-19 11:49:55 -0400

    moved everything to a single loop for efficiency; added return

--- R/parse_SNP.R
+++ R/parse_SNP.R
@@ -1,50 +1,55 @@
 parse_SNP <- function(all_data, LD) {
 
-  #Give upstream and downstream their own variables to make life easier
+  # give upstream and downstream their own variables to make life easier
   LD_upstream <- LD[[1]]
   LD_downstream <- LD[[2]]
 
+  # upstream loop
   for (name in names(LD_upstream)){
     temp_data <- LD_upstream[[name]]
-    LD_upstream[[name]] <- temp_data[(temp_data$Position1 %in% all_data$Pos),]
+    
+    # filter based on SNPs in stats/effects
+    temp_data <- temp_data[(temp_data$Position1 %in% all_data$Pos),]
+    
+    # retrieve linked SNPs
+    chr_linked <- temp_data %>% arrange(Position1) %>% filter(R.2 >= 0.8)
+    
+    # catch everything that didn't make it through the filter
+    chr_unlinked <- temp_data %>% filter(R.2 >= 0.8) %>% arrange(Position1, Dist_bp)
+    
+    # find the first instance of every unlinked SNP
+    chr_unlinked <- chr_unlinked[match(unique(chr_unlinked$Position1), chr_unlinked$Position1),]
+    
+    # combine linked and unlinked and arrange Position1 in ascending order
+    temp_data <- rbind(chr_linked, chr_unlinked) %>% arrange(Position1)
+    
+    # store modified and filtered data
+    LD_upstream[[name]] <- temp_data
   }
 
+  # downstream loop
   for (name in names(LD_downstream)){
     temp_data <- LD_downstream[[name]]
-    LD_downstream[[name]] <- temp_data[(temp_data$Position1 %in% all_data$Pos),]
-  }
-  
-  names(LD_upstream)
-  for (name in names(LD_upstream))
-  {
-    print(name)
-    print(LD_upstream[[1]]$Position1)
-  }
-  
-  for (chr in names(LD_upstream)){
-    chr_up <- LD_upstream[[chr]]
     
-    #Retrieve linked SNPs
-    chr_linked <- chr_up %>% arrange(Position1) %>% filter(R.2 >= 0.8)
+    # filter based on SNPs in stats/effects
+    temp_data <- temp_data[(temp_data$Position1 %in% all_data$Pos),]
     
-    #Catch everything that didn't make it through the filter
-    chr_unlinked <- chr_up %>% filter(R.2 >= 0.8) %>% arrange(Position1, Dist_bp)
+    # retrieve linked SNPs
+    chr_linked <- temp_data %>% arrange(Position1) %>% filter(R.2 >= 0.8)
     
-    #Find the first instance of every unlinked SNP
+    # catch everything that didn't make it through the filter
+    chr_unlinked <- temp_data %>% filter(R.2 >= 0.8) %>% arrange(Position1)
+    
+    # find the first instance of every unlinked SNP
     chr_unlinked <- chr_unlinked[match(unique(chr_unlinked$Position1), chr_unlinked$Position1),]
     
-    #Combine linked and unlinked and arrange Position in ascending order
-    chr_up <- rbind(chr_linked, chr_unlinked) %>% arrange(Position1)
+    # combine linked and unlinked and arrange Position1 in ascending order
+    temp_data <- rbind(chr_linked, chr_unlinked) %>% arrange(Position1)
+    
+    # store modified and filtered data
+    LD_downstream[[name]] <- temp_data
   }
   
-  #Same steps as the loop prior but for LD_downstream
-  for (chr in names(LD_downstream)){
-    chr_down <- LD_downstream[[chr]]
-    chr_linked <- chr_down %>% arrange(Position1) %>% filter(R.2 >= 0.8)
-    
-    #Only arrange by Position1 not Position1 and Dist_bp
-    chr_unlinked <- chr_down %>% filter(R.2 >= 0.8) %>% arrange(Position1)
-    chr_unlinked <- chr_unlinked[match(unique(chr_unlinked$Position1), chr_unlinked$Position1),]
-    chr_down <-  rbind(chr_linked, chr_unlinked) %>% arrange(Position1)
-  }
+  # return modified lists
+  list(LD_upstream, LD_downstream)
 }
