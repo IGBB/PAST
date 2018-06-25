125479d8678a14fad3f73b81743da9ed04090663
HugeLegolas <39808987+HugeLegolas@users.noreply.github.com>
2018-06-25 16:05:35 -0500

    Yes

--- parse_SNP.R
+++ parse_SNP.R
@@ -6,7 +6,8 @@ parse_SNP <- function(all_data, LD) {
 
   # upstream loop
   for (name in names(LD_upstream)){
-    temp_data <- LD_upstream[[name]]
+    temp_data <- LD_upstream[[name]] %>% mutate(Marker1 = paste0("S", Locus1, "_", Position1)) %>% 
+      mutate(Marker2 = paste0("S", Locus1, "_", Position2))
     
     # filter based on SNPs in stats/effects
     temp_data <- temp_data[(temp_data$Position1 %in% all_data$Pos),]
@@ -20,21 +21,26 @@ parse_SNP <- function(all_data, LD) {
     # find the first instance of every unlinked SNP
     chr_unlinked <- chr_unlinked[match(unique(chr_unlinked$Position1), chr_unlinked$Position1),]
     
+    block_SNPs <- chr_linked %>% group_by(Marker1) %>% summarise(count = n()) %>% filter(count > 1)
+    block_SNPs <- chr_linked %>% filter(Marker1 %in% block_SNPs$Marker1)
+    index <- c(0, cumsum(abs(diff(block_SNPs$Site2)) > 1))
+    blocks <- split(block_SNPs, index)
+  
+    test_data <- merge(block_SNPs, all_data, by.x = "Marker1", by.y = "Marker") %>%
+      select(Locus1, Position1, Position2, Site2, Dist_bp, R.2, p, Estimate.x, Marker2)
+    test_data <- merge(test_data, all_data, by.x = "Marker2", by.y = "Marker")
+    
+    test_data <- mutate(test_data, SNP1_pval = test_data$p)
+    test_data <- mutate(test_data, SNP1_effect = test_data$Estimate.x)
+    single_SNPs <- chr_linked %>% group_by(Position1) %>% summarise(count = n()) %>% filter(count == 1)
+    
+    ## Last two steps are below this line. All your stuff should be above this.
+    
     # combine linked and unlinked and arrange Position1 in ascending order
     temp_data <- rbind(chr_linked, chr_unlinked) %>% arrange(Position1)
     
     # store modified and filtered data
     LD_upstream[[name]] <- temp_data
-    
-    temp_data <- temp_data %>% mutate(Marker1 = paste0("S", Locus1, "_", Position1)) %>% 
-      mutate(Marker2 = paste0("S", Locus1, "_", Position2))
-    
-    index <- c(0, cumsum(abs(diff(temp_data$Site2)) > 1))
-    split(temp_data, index)
-    
-    block_SNPs <- chr_linked %>% group_by(Position1) %>% summarise(count = n()) %>% filter(count > 1)
-    single_SNPs <- chr_linked %>% group_by(Position1) %>% summarise(count = n()) %>% filter(count == 1)
-    
   }
 
   # downstream loop
