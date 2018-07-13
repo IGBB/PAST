f7392fedede3c3804bceb5ec73075bc00635e4ce
HugeLegolas <39808987+HugeLegolas@users.noreply.github.com>
2018-07-13 10:17:31 -0500

    Just because github is an annoying cranky
    guy that has to have everything exactly the way it wants because it's also a child..... I'm salty

--- R/parse_LD.R
+++ R/parse_LD.R
@@ -8,39 +8,27 @@
 #   Check Package:             'Ctrl + Shift + E'
 #   Test Package:              'Ctrl + Shift + T'
 
-library(dplyr)
 parse_LD <- function(LD_file) {
   LD_all<-read.table(LD_file, header=TRUE)
-
+  
   # filter out Locus1 == 0 and select certain columns and remove NaN
   LD_all<-filter(LD_all, Locus1 != 0) %>% select("Locus1", "Position1", "Site1", "Position2", "Site2", "Dist_bp", "R.2")
   LD_all<-LD_all[complete.cases(LD_all),]
-
+  
   # split by Locus1
   LD<-split(LD_all, f=LD_all$Locus1)
-
+  
   # already sorted by Site1
   LD_upstream<-LD
-
-  # swap Site2/Position2 and Site1/Position1
-  # for downstream, we treat SNP2 as SNP1
+  
+  # sort by Site2 for downstream
   LD_downstream<-LD
   for (name in names(LD_downstream)) {
-    temp_data<-LD_downstream[[name]] %>% 
-      mutate(temp_p2 = Position1, temp_s2 = Site1, Position1 = Position2, Site1 = Site2) %>%
-      mutate(Site2 = temp_s2, Position2 = temp_p2, temp_s2 = NULL, temp_p2 = NULL)
-    LD_downstream[[name]]<-temp_data[with(temp_data, order(Site1)),]
+    temp_data<-LD_downstream[[name]]
+    LD_downstream[[name]]<-temp_data[with(temp_data, order(Site2)),]
   }
-
+  
   # return list with upstream and downstream
   list(LD_upstream, LD_downstream)
 }
-
-
-
-
-
-
-
-
 
