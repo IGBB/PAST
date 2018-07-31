2ac619b3b168ab7e1544bc3fc6212b8f3957ec2e
Adam Thrash <cathrash92@gmail.com>
2018-07-31 08:55:22 -0400

    minor bug fixes, code style changes

--- R/parse_SNP.R
+++ R/parse_SNP.R
@@ -258,55 +258,50 @@ parse_SNP <- function(all_data, LD, gff_file, window) {
       
       unlinked_SNP_genes <- unlinked_SNP_genes %>% filter(name != "NA")
   
-      ## Last two steps are below this line. All your stuff should be above this.
-  
-      # combine linked and unlinked and arrange Position1 in ascending order
-      temp_data <- rbind(chr_linked, chr_unlinked) %>% arrange(Position1)
+      # find tagSNPs
+      all_genes <- rbind(block_genes, single_SNP_genes, unlinked_SNP_genes)
+      all_genes <- arrange(all_genes, desc(name))
+      all_genes <- all_genes %>% mutate(linkedSNP_count = ifelse(linkedSNP_count <= 0, 1 ,linkedSNP_count))
+      group_genes <- split(all_genes, f = all_genes$name)
+      for ( name_block in names(group_genes)){
+        single <- group_genes[[name_block]]
+        neg_genes <- single %>% filter(SNP2_effect < 0) %>% arrange(SNP2_effect)
+        pos_genes <- single %>% filter(SNP2_effect > 0) %>% arrange(desc(SNP2_effect))
+        negative <- sum(neg_genes$linkedSNP_count)
+        positive <- sum(pos_genes$linkedSNP_count)
+        
+        if (positive > negative){
+          single <- single %>% arrange(desc(SNP2_effect))
+          tagSNP <- single[1,]
+        } else if(negative > positive){
+          single <- single %>% arrange(SNP2_effect)
+          tagSNP <- single[1,]
+        } else if(positive == negative){
+          pos_max <- pos_genes[1,]
+          neg_max <- neg_genes[1,]
+          neg_max$SNP2_effect <- abs(neg_max$SNP2_effect)
+          
+          if(pos_max$SNP2_effect > neg_max$SNP2_effect){
+            tagSNP <- pos_max
+          }
+          else{
+            tagSNP <- neg_max
+          }
+        }
+        
+        # code to overwrite group_genes[[name_block]] with tagSNP
+      }
+      
+      # code to rejoin group_genes into a single dataframe
   
       # store modified and filtered data
-      LD_stream[[name]] <- temp_data
+      LD_stream[[name]] <- group_genes
   
     } # END UP/DOWNSTREAM LOOP
     
     # set up/downstream to modified data
     LD[[i]] <- LD_stream
     
-  }
-  # Gene sorting loop
-  all_genes <- rbind(block_genes, single_SNP_genes, unlinked_SNP_genes)
-  all_genes <- arrange(all_genes, desc(name))
-  all_genes <- all_genes %>% mutate(linkedSNP_count <- ifelse(linkedSNP_count <= 0, 1 ,linkedSNP_count))
-  group_genes <- split(all_genes, f = all_genes$name)
-  for ( name_block in names(group_genes)){
-    single <- group_genes[[name_block]]
-    neg_genes <- single %>% filter(SNP2_effect < 0)
-    pos_genes <- single %>% filter(SNP2_effect > 0)
-    negative <- sum(neg_genes$linkedSNP_count)
-    positive <- sum(pos_genes$linkedSNP_count)
-    
-    if (positive > negative){
-      single <- single %>% arrange(desc(SNP2_effect))
-      tagSNP <- single[1,]
-  } else if(negative > positive){
-    single <- single %>% arrange(SNP2_effect)
-    tagSNP <- single[1,]
-  } else if(positive == negative){
-   pos_genes <- pos_genes %>% arrange(desc(SNP2_effect))
-   pos_max <- pos_genes[1,]
-   
-   neg_genes <- neg_genes %>% arrange(SNP2_effect)
-   neg_max <- neg_genes[1,]
-   
-   neg_max$SNP2_effect <- abs(neg_max$SNP2_effect)
-   
-   if(pos_max$SNP2_effect > neg_max$SNP2_effect){
-     tagSNP <- pos_max
-   }
-   else{
-     tagSNP <- neg_max
-   }
-      
-  }
   }
   
   # return LD
