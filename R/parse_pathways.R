dced58621d5585d9c7a9787310f15aa2558991b0
Adam Thrash <cathrash92@gmail.com>
2018-08-03 11:45:42 -0400

    added more skeleton code

--- R/parse_pathways.R
+++ R/parse_pathways.R
@@ -1,4 +1,4 @@
-parse_pathways <- function(LD, pathways_file) {
+parse_pathways <- function(tagSNPs, pathways_file) {
   
   # debugging code
   pathways_file = "example/pathways.txt"
@@ -6,7 +6,28 @@ parse_pathways <- function(LD, pathways_file) {
   # load pathways
   pathways <- read.table(pathways_file, sep = "\t", header = TRUE, quote="")
   
-  # merge pathways and LD by gene
+  # UP/DOWNSTREAM LOOP
+  for (i in 1:length(tagSNPs)) {
+    tagSNPs_stream <- tagSNPs[[i]]
+    names(LD_stream)
+    
+    # BEGIN PROCESSING BY CHROMOSOMES LOOP
+    for (name in names(tagSNPs_stream)) {
+      temp_tagSNPs <-tagSNPs_stream[[name]] 
+      
+      # merge pathways and temp_tagSNPs by gene
+      # important functions: merge
+      # reference: parse_SNP.R, line 147
+      
+      # filter out pathways with less than five genes associated with them
+      # important functions: group_by, summary, filter
+      # reference: parse_SNP.R, line 118-120
+      
+    } # END CHROMOSOMES LOOP
+    
+  } # END UP/DOWNSTREAM LOOP
   
-  # filter out pathways with less than five genes associated with them
+  
+  
+  
 }
